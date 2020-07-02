810d6e393a6a14295b048ca7a8fbe28910fe1bac
skelorc <skelorc@yandex.ru>
2020-07-02 18:24:21 +0300

    Work on the bugs

--- app/src/main/java/com/test/myapplication/MainActivity.java
+++ app/src/main/java/com/test/myapplication/MainActivity.java
@@ -7,19 +7,16 @@ import android.content.ActivityNotFoundException;
 import android.content.ClipData;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
-import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
@@ -30,8 +27,9 @@ public class MainActivity extends AppCompatActivity {
 
     private final String URL = "https://navsegda.net/";
     WebView webView;
-    MyWebViewClient webViewClient = new MyWebViewClient();
+
     MyWebChromeClient myWebChromeClient = new MyWebChromeClient();
+    SharedPreferences prefs;
 
     private ValueCallback<Uri> uploadMessage;
     private ValueCallback<Uri[]> uploadMessageL;
@@ -50,26 +48,21 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setLoadsImagesAutomatically(true);
         webSettings.setJavaScriptEnabled(true);
         webSettings.setAllowFileAccess(true);
-        webView.setWebViewClient(webViewClient);
+        webView.setWebViewClient(new WebViewClient());
         webView.setWebChromeClient(myWebChromeClient);
         webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
-        webView.loadUrl(URL);
 
-//Закомменитил, так как прописал в манифесте konfig.
-       /* if (savedInstanceState != null) {
-            webView.restoreState(savedInstanceState.getBundle("webViewState"));
-        } else
-
-        {
+        prefs = this.getSharedPreferences(this.getPackageName(),
+                Activity.MODE_PRIVATE);
+        String s = prefs.getString("lastUrl", "");
+        if(s.equals("")) {
+            webView.loadUrl(s);
+        }
+        else
+            {
             webView.loadUrl(URL);
-        }*/
+        }
     }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-    }
-
 
 
 
@@ -80,38 +73,8 @@ public class MainActivity extends AppCompatActivity {
                 getSharedPreferences(this.getPackageName(), Activity.MODE_PRIVATE);
         SharedPreferences.Editor edit = prefs.edit();
         edit.putString("lastUrl",webView.getUrl());
-        edit.commit();
+        edit.apply();
     }
-
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        if (webView != null) {
-            SharedPreferences prefs = this.
-                    getSharedPreferences(this.getPackageName(), Activity.MODE_PRIVATE);
-            String s = prefs.getString("lastUrl", "");
-            if (!s.equals("")) {
-                webView.loadUrl(s);
-            }
-        }
-    }
-
-
-  /*  @Override
-    protected void onSaveInstanceState(Bundle outState) {
-        Bundle bundle = new Bundle();
-        outState.putBundle("webViewState", bundle);
-        webView.saveState(outState);
-        super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        webView.restoreState(savedInstanceState);
-        super.onRestoreInstanceState(savedInstanceState);
-    }
-*/
 
     @Override
     public void onBackPressed() {
@@ -122,21 +85,6 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class MyWebViewClient extends WebViewClient {
-        @TargetApi(Build.VERSION_CODES.N)
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            view.loadUrl(request.getUrl().toString());
-            return true;
-        }
-
-        // Для api < 24
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
-    }
 
     private class MyWebChromeClient extends WebChromeClient {
 
@@ -144,24 +92,24 @@ public class MainActivity extends AppCompatActivity {
         @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
         public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
 
-            if (uploadMessageL != null) {
+            if (uploadMessageL != null)
+            {
                 uploadMessageL.onReceiveValue(null);
                 uploadMessageL = null;
             }
 
             uploadMessageL = filePathCallback;
-
             Intent intent = fileChooserParams.createIntent();
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("image/*");
             try {
-                startActivityForResult(intent, FILECHOOSER_RESULT_CODE);
+                startActivityForResult(Intent.createChooser(intent, "File Chooser"), FILECHOOSER_RESULT_CODE);
             } catch (ActivityNotFoundException e) {
                 uploadMessageL = null;
                 return false;
             }
             return true;
         }
-
-
         // Для API Android <11 (ОС 3.0)
         protected void openFileChooser(ValueCallback uploadMsg, String acceptType) {
             uploadMessage = uploadMsg;
